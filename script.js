c1fa6ca02e15f19ac90c366c46e99bb8dcff2921
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-08-31 01:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+const form1 = document.getElementById("form1");
+
+form1.addEventListener('submit', fetchData);
+
+function fetchData(e){
+	e.preventDefault();
+	const name = document.getElementById("name").value;
+	const age = document.getElementById("age").value;
+
+	const prom = new Promise((resolve,reject)=>{
+		      if(age >=18){
+				  setTimeout(()=>{
+					resolve();  
+				  },4000);
+			  }
+		else{
+			setTimeout(()=>{
+					reject();  
+				  },4000);
+		   }
+	     });
+	prom.then(()=>{
+		alert(`Welcome, ${name}. You can vote`);
+	})
+	.catch(()=>{
+		alert(`Oh sorry, ${name}. You aren't old enough`);
+	});
+}
+
